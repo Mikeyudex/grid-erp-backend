cd14a196fadaec088960c4b6c9a2e7d659754bbe
Mikeyudex <miguel.yudex@gmail.com>
2025-05-10 09:09:41 -0500

    se modifica el archivo de despliegue

--- deploy.dev.sh
+++ deploy.dev.sh
@@ -15,7 +15,7 @@ npm run build
 # deploy
 echo "Deploying..." >> $FILE_LOG
 if pm2 list | grep -q 'online'; then #verifica si hay un servicio corriendo
-    pm2 reload ecosystem.config.dev.js --env dev
+    pm2 reload ecosystem.config.dev.json --env dev
 else
     npm run deploy:dev
 fi
