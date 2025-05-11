abb5d53cd103a3274f2c5c84e96301c85d8e6058
Mikeyudex <miguel.yudex@gmail.com>
2025-05-10 19:21:06 -0500

    se actualiza la configuración de CORS en el bootstrap de la aplicación

--- apps/core/src/main.ts
+++ apps/core/src/main.ts
@@ -30,6 +30,33 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.useWebSocketAdapter(new WsAdapter(app));
+   app.enableCors({
+      origin: (origin, callback) => {
+        const allowedOrigins = [];
+        if (process.env.NODE_ENV === 'local' || process.env.ENV === 'LOCAL') {
+          allowedOrigins.push('http://localhost:3000');
+          allowedOrigins.push('http://localhost:3001');
+          allowedOrigins.push('http://localhost:3002');
+        } else if (process.env.NODE_ENV === 'dev') {
+          allowedOrigins.push('http://149.130.186.128:8080');
+        }
+        else if (process.env.NODE_ENV === 'prod') {
+          allowedOrigins.push('http://149.130.186.128:8080');
+        } else {
+          allowedOrigins.push('http://149.130.186.128:8080');
+        }
+        if (!origin || allowedOrigins.includes(origin)) {
+          callback(null, true);
+        } else {
+          console.log("Allowed origins: " + allowedOrigins);
+          console.log("Origin: " + origin);
+          callback(new Error('No permitido por CORS'));
+        }
+      },
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+      credentials: true,
+    });
+  
   await app.listen(configsService.get('PORT'));
   if (module.hot) {
     module.hot.accept();
